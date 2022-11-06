5ec921cb0999e5cd1211a7bd944fed1e28ae9910
jv <jvandersson93@gmail.com>
2022-11-06 01:06:51 -0500

    add funcionalidad method opcion5 tarea 2

--- archivo2.py
+++ archivo2.py
@@ -92,6 +92,32 @@ def opcion4():
 
 def opcion5():
     print('Has elegido la opción 5')
+    api_pokemon_tipo = "https://pokeapi.co/api/v2/type/"
+    contar = 2
+    response_tipo = requests.get(api_pokemon_tipo)
+    data_tipo = response_tipo.json()
+    print("****Lista de Tipos de Pokemons****")
+    for data in range(0, len(data_tipo['results']) ,contar):
+        print(f"[{data+1}]{data_tipo['results'][data]['name']:<21} [{data+2}]{data_tipo['results'][data+1]['name']:<19}")
+    num_tipo = lee_entero("Seleccione un tipo de pokemon:")
+    while (int(num_tipo) == 0) or (int(num_tipo) > 20):
+        print("¡Ha seleccionado un tipo fuera del rango")
+        num_tipo = lee_entero("Seleccione un tipo de pokemon:")
+    print(api_pokemon_tipo + str(int(num_tipo)-1) + "/")
+    response_tipo_pokemon = requests.get(api_pokemon_tipo + str(num_tipo) + "/")
+    data_tipo_pokemon = response_tipo_pokemon.json()  
+    print(f"****Lista de Pokemones****")
+    for pokemon in data_tipo_pokemon['pokemon']:
+        response_pokemon = requests.get(pokemon['pokemon']['url'])
+        data_pokemon = response_pokemon.json()
+        MiPokemon = Pokemon(
+            data_pokemon["name"],
+            list(habilidad["ability"]["name"] for habilidad in data_pokemon["abilities"]),
+            data_pokemon["sprites"]['front_default']
+        )
+        MiPokemon.mostrar_datos()
+        print("-"*50)
+
 
 def salir():
     print('Saliendo')
